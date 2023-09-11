37172438651ba4b59a0daae9fe1392e395289633
AmbikaValagonda <ambika.kh@gmail.com>
2023-09-11 12:04:36 -0500

    Initial Commit

---
+++ src/operators/OperatorIncrementDecrement.java
@@ -0,0 +1,34 @@
+package operators;
+
+public class OperatorIncrementDecrement {
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b;
+		int c;
+		a++;
+		++a;
+		++a;
+		a++;
+		System.out.println(a);
+		a = ++a;
+		System.out.println(a);	
+		a = a++;
+		System.out.println(a);
+		c = ++a;
+		System.out.println(c);
+		System.out.println(a);
+		//Ex10 Compound assignment - while assigning we are doing the operation
+		int age = 18;
+		age += 18;
+		System.out.println(age);
+		
+		int num = 10;
+		num -= 1;
+		System.out.println(num);
+		
+		
+		
+	}
+
+}
---
+++ src/operators/RelationalLogicalOperators.java
@@ -0,0 +1,53 @@
+package operators;
+
+public class RelationalLogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// <, <=, >, >=, !=, == Relational
+		// &&, ||, !, ~ Logical
+		// int 12age; Invalid
+		int age12; // Valid
+		int age_12_; //Valid
+		int $; //Valid
+		int $_; //Valid
+		int _$; //Valid
+		int $Age; //Valid
+		int _$age; //Valid
+		int m_of_$; //Valid
+		int num = 10_1000; //Valid
+		//int 10_; //Invalid
+		float f = 10.5f;
+		//float f1 = 10.5_f; //Invlid
+		int ff_45;//Valid
+		//int ff_@; //Invalid
+		int ff_; //Valid
+			
+		
+		int a = 18;
+		int b = 20;
+		int c = 18;
+		
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a==b);
+		System.out.println(a<=b);
+		System.out.println(a>=c);
+		System.out.println(a!=c);
+		System.out.println(a==b);
+		System.out.println(a==c);
+		
+		boolean res = (a<b) && (a>b) && (a==c);
+		System.out.println("res:: "  + res);
+		
+		boolean res1 = (a<b) || (a>b) || (a==c);
+		System.out.println("res1:: "  + res1);
+	
+		boolean res2 = (a<b) && (a<=b) && (a>=c);
+		System.out.println("res2:: "  + res2);
+		
+		
+		
+	}
+
+}
